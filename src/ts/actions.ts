688827f3aabd0a4a14e2711ad660dbba9fdbc93e
Bykov Denis Stanislavovich <stivenrain@gmail.com>
2024-08-13 23:09:44 +0300

    #3 Improve functionality

--- src/ts/actions.ts
+++ src/ts/actions.ts
@@ -1,19 +1,53 @@
+import 'virtual:svg-icons-register';
+
 window.addEventListener('load', () => {
   
+  const mainBodyBlock: HTMLElement | null = document.querySelector<HTMLElement>('.main-body');
   const sliderBlock: HTMLElement | null = document.querySelector<HTMLElement>('#slider');
   const participantsBlock: HTMLElement | null = document.querySelector<HTMLElement>('#participants');
   const judgesBlock: HTMLElement | null = document.querySelector<HTMLElement>('#judges');
   const ticketsBlock: HTMLElement | null = document.querySelector<HTMLElement>('#tickets');
   const contactUsBlock: HTMLElement | null = document.querySelector<HTMLElement>('#contact-us');
+  const mainNavItems: NodeListOf<HTMLElement> | null = document.querySelectorAll<HTMLElement>('.nav-items__link');
+  const hashtagElement: HTMLElement | null = document.querySelector<HTMLElement>('.logo-container__hashtag');
+  const langActualElement: HTMLElement | null = document.querySelector<HTMLElement>('.lang__actual');
+  
+  function changeStyleMainNav(isVisible: boolean) {
+    if (hashtagElement) {
+      if (isVisible) {
+        hashtagElement.classList.add('logo-container__hashtag--invisible');
+      } else {
+        hashtagElement.classList.remove('logo-container__hashtag--invisible');
+      }
+    }
+    if (langActualElement) {
+      if (isVisible) {
+        langActualElement.classList.add('lang__actual--main-page');
+      } else {
+        langActualElement.classList.remove('lang__actual--main-page');
+      }
+    }
+    if (mainNavItems) {
+      for (let element of mainNavItems) {
+        if (element) {
+          if (isVisible) {
+            element.classList.add('link-button--main-page');
+          } else {
+            element.classList.remove('link-button--main-page');
+          }
+        }
+      }
+    }
+  }
   
   if (sliderBlock) {
     sliderBlock.style.display = 'none';
   }
-  if (participantsBlock) {
-    participantsBlock.style.display = 'none';
-  }
   if (judgesBlock) {
     judgesBlock.style.display = 'none';
+  }
+  if (ticketsBlock) {
+    ticketsBlock.style.display = 'none';
   }
   if (contactUsBlock) {
     contactUsBlock.style.display = 'none';
@@ -38,7 +72,9 @@ window.addEventListener('load', () => {
           judgesLinkButtonElement?.classList.remove('link-button--active');
           ticketsLinkButtonElement?.classList.remove('link-button--active');
           contactsLinkButtonElement?.classList.remove('link-button--active');
-          
+          if (mainBodyBlock) {
+            mainBodyBlock.classList.add('main-body--main-page')
+          }
           if (sliderBlock) {
             sliderBlock.style.display = 'block';
           }
@@ -55,6 +91,8 @@ window.addEventListener('load', () => {
             contactUsBlock.style.display = 'none';
           }
           
+          changeStyleMainNav(true);
+          
           break;
         }
         case 'participants-link-button': {
@@ -64,6 +102,9 @@ window.addEventListener('load', () => {
           ticketsLinkButtonElement?.classList.remove('link-button--active');
           contactsLinkButtonElement?.classList.remove('link-button--active');
           
+          if (mainBodyBlock) {
+            mainBodyBlock.classList.remove('main-body--main-page')
+          }
           if (sliderBlock) {
             sliderBlock.style.display = 'none';
           }
@@ -79,6 +120,7 @@ window.addEventListener('load', () => {
           if (contactUsBlock) {
             contactUsBlock.style.display = 'none';
           }
+          changeStyleMainNav(false);
           break;
         }
         case 'judges-link-button': {
@@ -88,6 +130,9 @@ window.addEventListener('load', () => {
           ticketsLinkButtonElement?.classList.remove('link-button--active');
           contactsLinkButtonElement?.classList.remove('link-button--active');
           
+          if (mainBodyBlock) {
+            mainBodyBlock.classList.remove('main-body--main-page')
+          }
           if (sliderBlock) {
             sliderBlock.style.display = 'none';
           }
@@ -103,6 +148,7 @@ window.addEventListener('load', () => {
           if (contactUsBlock) {
             contactUsBlock.style.display = 'none';
           }
+          changeStyleMainNav(false);
           break;
         }
         case 'tickets-link-button': {
@@ -112,6 +158,9 @@ window.addEventListener('load', () => {
           judgesLinkButtonElement?.classList.remove('link-button--active');
           contactsLinkButtonElement?.classList.remove('link-button--active');
           
+          if (mainBodyBlock) {
+            mainBodyBlock.classList.remove('main-body--main-page')
+          }
           if (sliderBlock) {
             sliderBlock.style.display = 'none';
           }
@@ -127,6 +176,7 @@ window.addEventListener('load', () => {
           if (contactUsBlock) {
             contactUsBlock.style.display = 'none';
           }
+          changeStyleMainNav(false);
           break;
         }
         case 'contacts-link-button': {
@@ -136,6 +186,9 @@ window.addEventListener('load', () => {
           judgesLinkButtonElement?.classList.remove('link-button--active');
           ticketsLinkButtonElement?.classList.remove('link-button--active');
           
+          if (mainBodyBlock) {
+            mainBodyBlock.classList.remove('main-body--main-page')
+          }
           if (sliderBlock) {
             sliderBlock.style.display = 'none';
           }
@@ -151,13 +204,11 @@ window.addEventListener('load', () => {
           if (contactUsBlock) {
             contactUsBlock.style.display = 'block';
           }
+          changeStyleMainNav(false);
           break;
         }
       }
     })
-    
-    
-    
   }
   
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,61 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import ViteWebp from 'vite-plugin-webp-generator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+      port: 3000
+    },
     build: {
         rollupOptions: {
             input: {
                 main: path.join(__dirname, 'index.html'),
             },
+          output: {
+            assetFileNames: (assetInfo: any) => {
+              var info = assetInfo.name.split(".");
+              var extType = info[info.length - 1];
+              if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                extType = "img";
+              } else if (/woff|woff2/.test(extType)) {
+                extType = "fonts";
+              }
+              return `static/${extType}/[name]-[hash][extname]`;
+            },
+            chunkFileNames: "static/js/[name]-[hash].js",
+            entryFileNames: "static/js/[name]-[hash].js",
+          },
         },
+      outDir: './dist'
     },
+  plugins: [
+    ViteWebp.default({
+      extensions: ["png", "jpg"]
+    }),
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), 'src/img/icons')],
+      // Specify symbolId format
+      symbolId: 'icon-[dir]-[name]',
+      inject: 'body-last'
+    }),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 70,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 70,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 70,
+      },
+    })
+  ],
 })
